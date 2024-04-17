89782a7d42636645e6634262a7b5f1c9bfd7fa75
yiwen0328 <111B13910@mailst.cjcu.edu.tw>
2024-04-18 00:12:55 +0800

    點餐系統

--- app/src/main/java/com/example/test0417/MainActivity.java
+++ app/src/main/java/com/example/test0417/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.test0417;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button systemButton = findViewById(R.id.button);
+        systemButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                showMultiChoiceDialog();
+            }
+        });
+    }
+
+    private void showMultiChoiceDialog() {
+        final String[] items = new String[]{getString(R.string.yummy), getString(R.string.sausage), getString(R.string.fruit), getString(R.string.banana), getString(R.string.chicken)};
+        final boolean[] checkedItems = {false, false, false, false, false};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.selected_items); // 使用新的字符串資源
+        builder.setMultiChoiceItems(items, checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                checkedItems[which] = isChecked;
+            }
+        });
+        builder.setPositiveButton("確認", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Handle the selected items
+            }
+        });
+        builder.setNegativeButton("離開", null);
+        builder.show();
     }
 }
